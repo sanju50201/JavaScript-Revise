1c4d4c17c7ce5cf3c57a548951c179923bb0a14d
sanju50201 <sanjithk50201@gmail.com>
2022-10-13 20:38:48 +0530

    classes

--- Phase 1/debug.js
+++ Phase 1/debug.js
@@ -23,10 +23,44 @@
 // console.log('HELLO!');
 // makeSome('I LOVE PIZZA', document.body);
 
-const moveX = (element, amount, delay) => {
-  setTimeout(() => {
-    element.style.transform = `translateX${amount}px`;
-  }, delay);
+// const moveX = (element, amount, delay) => {
+//   setTimeout(() => {
+//     element.style.transform = `translateX${amount}px`;
+//   }, delay);
+// };
+
+// moveX(button, 600, 2000);
+
+// if a function starts with uppercase letters it is a constructor function
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+  console.log(this); // this is now referring to this function
+}
+
+// this method is now defined in the prototype
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r},${g},${b})`;
+};
+// this method is now defined in the prototype
+
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 };
 
-moveX(button, 600, 2000);
+Color.prototype.rgba = function (a = 1.0) {
+  const { r, g, b } = this;
+  return `rgba(${r},${g},${b},${a})`;
+};
+
+const color1 = new Color(56, 75, 21);
+
+const color2 = new Color(0, 0, 0);
+
+color1.hex();
+
+// Classes
--- Phase 1/script.js
+++ Phase 1/script.js
@@ -2001,3 +2001,199 @@ async function getPokemon() {
   }
 }
 getPokemon();
+
+// JavaScripts OOP
+
+// What is a prototype -- mechanism feature which inherit features from one other
+
+String.prototype.yell = function () {
+  return `${this.toUpperCase()} !!!!!, YOU SUCK!!!`;
+};
+String.prototype.toUpperCase = function () {
+  return 'you are lower case 🤣🤣';
+};
+
+// Factory functions
+
+// function makeColor(r, g, b) {
+//   const color = {};
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r},${g},${b})`;
+//   };
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
+//   return color;
+// }
+// const firstColor = makeColor(35, 255, 150);
+// firstColor.hex();
+// firstColor.rgb();
+
+// const black = makeColor(0, 0, 0);
+// black.rgb();
+// black.hex();
+
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+//   console.log(this);
+// }
+
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+//   console.log(this); // this is now referring to this function
+// }
+
+// // this method is now defined in the prototype
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r},${g},${b})`;
+// };
+// this method is now defined in the prototype
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r},${g},${b},${a})`;
+// };
+
+// const color1 = new Color(56, 75, 21);
+
+// const color2 = new Color(0, 0, 0);
+
+// color1.hex();
+
+// Classes Syntactic sugar
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+    this.calcHSL();
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `rgb(${r},${g},${b})`;
+  }
+  rgb() {
+    this.innerRGB();
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()},${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h},${s}%, ${l}%)`;
+  }
+  fullySaturated() {
+    const { h, l } = this;
+    return `hsl(${h},100%, ${l}%)`;
+  }
+  opposite() {
+    const { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue},${s}%, ${l}%)`;
+  }
+  calcHSL() {
+    let { r, g, b } = this;
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      // Red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
+}
+
+const brown = new Color(122, 75, 21, 'brown');
+const black = new Color(0, 0, 0, 'black');
+
+// extends
+class Animals {
+  constructor(name, age) {
+    console.log('IN PET CONSTRUCTOR');
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating`;
+  }
+}
+// this is extended by the animals class
+class Pet extends Animals {
+  allAreAnimals() {
+    console.log('THEY ARE PET ANIMALS');
+  }
+}
+
+class Dogs extends Pet {
+  woof() {
+    return 'woof!';
+  }
+  // if a method is not present here then it calls the parent class, else it executes which is present
+  eat() {
+    return `${this.name} is hungry!`;
+  }
+}
+
+class Cats extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    console.log('IN CAT CONSTRUCTOR');
+    super(name, age); // this references to the parent class
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return 'meow';
+  }
+}
